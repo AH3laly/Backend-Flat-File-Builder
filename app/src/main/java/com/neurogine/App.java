7edfe531665bc1698c0f868352b6e58fed8075db
Ah3laly <Ah3laly@gmail.com>
2024-05-12 01:55:56 +0800

    Implement startup code

--- app/src/main/java/com/neurogine/App.java
+++ app/src/main/java/com/neurogine/App.java
@@ -1,8 +1,37 @@
 package com.neurogine;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.neurogine.beans.FlatFileRecord;
+import com.neurogine.services.FlatFileService;
+import com.neurogine.services.FlatFileServiceImpl;
+
 public class App {
 
     public static void main(String[] args) {
-        
+    	
+    	FlatFileService flatFileService = new FlatFileServiceImpl();
+    	
+    	flatFileService.loadMappingFile();
+    	flatFileService.generate(generateDemoRecords());
+
+    }
+    
+    
+    private static List<FlatFileRecord> generateDemoRecords() {
+    	
+    	List<FlatFileRecord> demoDateList = new ArrayList<FlatFileRecord>();
+    	
+    	
+    	
+    	for(Integer i = 1; i < 20; i++) {
+    		FlatFileRecord tempF = new FlatFileRecord();
+    		tempF.setAMOUNT(String.valueOf(10.01 + (float)i));
+    		tempF.setREFERENCE_NO("6813162459".concat(String.valueOf(i)));
+    		demoDateList.add(tempF);
+    	}
+    	
+    	return demoDateList;
     }
 }
