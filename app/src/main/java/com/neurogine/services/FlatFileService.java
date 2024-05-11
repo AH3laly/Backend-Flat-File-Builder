46129dd77922931a829c7fe66fd73352466dd211
Ah3laly <Ah3laly@gmail.com>
2024-05-12 00:17:56 +0800

    Create FlatFileService

---
+++ app/src/main/java/com/neurogine/services/FlatFileService.java
@@ -0,0 +1,15 @@
+package com.neurogine.services;
+
+import java.util.List;
+
+import com.neurogine.beans.FlatFileRecord;
+
+public interface FlatFileService {
+	public static final String MAPPING_FILE_NAME = "mapping.xml";
+	public static final String MAPPING_FILE_STREAM_NAME = "flatFileMainStream";
+	public static final String OUTPUT_FILE_NAME = "flat_output.txt";
+	public void loadMappingFile();
+	public void loadMappingFile(String sourceFile);
+	public void generate(List<FlatFileRecord> content);
+	public void generate(List<FlatFileRecord> content, String destinationFile);
+}
